e22bbe1570bfee0c4ee58e2b0a093403d6fc3f18
yuvi-mittal <mittalyuvi02@gmail.com>
2025-11-01 19:38:16 +0530

    sum of three

---
+++ binary_search_tree/pointers/3sum_closest.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for(int i = 0; i < n - 2; i++) {
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(abs(target - sum) < abs(target - closest))
+                    closest = sum;
+
+                if(sum < target)
+                    j++;
+                else if(sum > target)
+                    k--;
+                else
+                    return target;
+            }
+        }
+        return closest;
+    }
+};
