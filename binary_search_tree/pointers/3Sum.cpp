bc9c8c5c4c781e74b6f490274e8ef01ce67ad9be
yuvi-mittal <mittalyuvi02@gmail.com>
2025-11-01 19:02:03 +0530

    sum of three

---
+++ binary_search_tree/pointers/3Sum.cpp
@@ -0,0 +1,39 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        int size = nums.size();
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<size;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int j= i+1;
+            int k = size-1;
+
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+
+                if(sum<0){
+                    j++;
+                }else if(sum >0){
+                    k--;
+                }else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while(j < k && nums[j] == nums[j+1]) j++;
+                    while(j < k && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
+                    
+                }
+            }
+        }
+                    return ans;
+
+    }
+};
